4e281925b2f907a251daa6cd5f4d6b224a75edd0
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-10 21:45:28 +0000

    implemented basic permission features that dont allow access to urls if user is not logged in and also dont allow deletion or editing of urls if user not logged in

--- express_server.js
+++ express_server.js
@@ -13,7 +13,7 @@ const urlDatabase = {
   i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" },
 };
 
-const users = { 
+const users = {
   userRandomID: {
     id: "userRandomID",
     email: "user@example.com",
@@ -77,6 +77,16 @@ function generateRandomString() {
   );
 }
 
+
+const urlsForUser = function(id, database){
+let userUrls = {};
+
+for (let shortURL in database){
+  if (database[shortURL].userID === id){
+    userUrls[shortURL] = database[shortURL]
+  }
+} return userUrls;
+}
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
@@ -87,12 +97,13 @@ app.get("/", (req, res) => {
 //list of URLs in database// homepage once user logs in
 app.get("/urls", (req, res) => {
   const userId = req.cookies["user_id"];
-
   const user = users[userId];
-  // console.log(mainUser.id);
-  console.log(urlDatabase);
-  const templateVars = { user: user, urls: urlDatabase };
+  const userUrls = urlsForUser(userId, urlDatabase)
+  const templateVars = { urls: userUrls, user };
 
+  if (!userId){
+    res.status(401).send("Unauthorized access, please login first")
+  }
   res.render("urls_index", templateVars);
 });
 
@@ -114,41 +125,76 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
+  console.log("HERE IS THE GET REQ")
   const userId = req.cookies["user_id"];
+  const shortURL = req.params.shortURL;
+   const longURL = urlDatabase[req.params.shortURL].longURL
   const user = users[userId];
+  const userUrls = urlsForUser(userId, urlDatabase)
+  if (!urlDatabase[shortURL]){
+    res.status(401).send("Unauthorized access, please login first")
+  }
+  else if (!userId || !userUrls[shortURL]) {
+
+    res.status(404).send("This shortURL does not exist")
+  } else{
   const templateVars = {
-    user:user,
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL].longURL,
+    user,
+    shortURL,
+    userUrls,
+    urlDatabase,
+    longURL
   };
 
   res.render("urls_show", templateVars);
-});
+}});
 // page for the newly generated short URL
 app.post("/urls", (req, res) => {
+  console.log(req.cookies["user_id"]);
   let longURL = req.body["longURL"];
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = { longURL: longURL, userID: req.cookies.user_id };
-  res.redirect(`/urls/${shortURL}`);
-});
+ 
+  res.redirect(`/urls/${shortURL}`);}
+ 
+);
 //redirecting to the website once clicked on the new generated shortURL
 app.get("/u/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
   let longURL = `${urlDatabase[req.params.shortURL]}`;
-  res.redirect(longURL);
+  res.redirect(`/urls/${shortURL}`);
 });
-
 //redirecting to the urls page once clicked on delete for a specific short url
 app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const userId = req.cookies["user_id"]
+  console.log('THIS IS USER ID ' + userId)
+console.log('THIS IS :'+ shortURL)
+console.log(urlDatabase[shortURL])
+
+  if (urlDatabase[shortURL].userID !== userId) {
+    res.status(401).send("You do not have access to do this")
+  } 
+  else {
   delete urlDatabase[req.params.shortURL];
-  res.redirect(`/urls`);
+  res.redirect(`/urls`);}
 });
 
 //redirects to the existing shortURL page where user can edit the short URL to correspond to a new Long URL
 app.post("/urls/:shortURL", (req, res) => {
+
+  console.log("ACCESSING POST EDIT ROUTE")
+  const userId = req.cookies["user_id"];
   const longURL = req.body["longURL"];
   const shortURL = req.params.shortURL;
   urlDatabase[shortURL].longURL = longURL;
-  res.redirect(`/urls`);
+ // urlDatabase[shortURL].longURL.userID = userId;
+
+  if (urlDatabase[shortURL] && urlDatabase[shortURL].userID === userId) {
+  res.redirect(`/urls`); }
+  else {
+    res.status(401).send("You dont have access to do this!")
+  }
 });
 
 //adding an endpoint to handle a POST to /login
@@ -194,17 +240,14 @@ app.post("/register", (req, res) => {
   }
   let user = userExistsByEmail(email);
   let id;
-  console.log(user);
   if (user) {
     return res.status(400).send("User already exists");
   }
   id = generateRandomString();
-  // console.log(newUserID);
   user = { id, email, password };
 
   users[id] = user;
   res.cookie("user_id", id);
-  // console.log(users[id])
   res.redirect("/urls");
 });
 
